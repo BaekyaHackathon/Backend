2414f1186b80188900af61dd15c7e940df68384f
Jinyshin <iwbm312@gmail.com>
2025-01-18 20:39:06 +0900

    Initial commit

---
+++ src/test/java/com/x8/digischool/DigiSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x8.digischool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
